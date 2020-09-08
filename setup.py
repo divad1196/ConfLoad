4c829c78b09acf451cf79b4810990892025ed20f
David Gallay <davidtennis96@hotmail.com>
2020-09-08 22:26:32 +0200

    correct setup version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='confload',
-    version='0.1',
+    version='0.2',
     author="Gallay David",
     author_email="davidtennis96@hotmail.com",
     description="A tool to handle configuration",
